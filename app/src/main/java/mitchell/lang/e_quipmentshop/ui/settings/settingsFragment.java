a04ff7219e49f2210f2bc8b64e5ed78f30b2d10f
Mitch <mitchlang444@gmail.com>
2019-11-10 20:14:44 -0500

    Created empty fragments for each page

---
+++ app/src/main/java/mitchell/lang/e_quipmentshop/ui/settings/settingsFragment.java
@@ -0,0 +1,31 @@
+package mitchell.lang.e_quipmentshop.ui.settings;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mitchell.lang.e_quipmentshop.R;
+
+
+public class settingsFragment extends Fragment {
+
+
+    public settingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+}
---
+++ app/src/main/java/mitchell/lang/e_quipmentshop/ui/stickpick/stickPickFragment.java
@@ -0,0 +1,30 @@
+package mitchell.lang.e_quipmentshop.ui.stickpick;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mitchell.lang.e_quipmentshop.R;
+
+
+public class stickPickFragment extends Fragment {
+
+
+    public stickPickFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stick_pick, container, false);
+    }
+
+}
