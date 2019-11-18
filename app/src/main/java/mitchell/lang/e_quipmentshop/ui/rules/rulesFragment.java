495dc316d8bff5daa8d5ed00e7362344ea2c0306
Mitch <mitchlang444@gmail.com>
2019-11-17 21:26:56 -0500

    #5, #6 Rules page edit/design and colour theme for app created

--- app/src/main/java/mitchell/lang/e_quipmentshop/ui/rules/rulesFragment.java
+++ app/src/main/java/mitchell/lang/e_quipmentshop/ui/rules/rulesFragment.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import mitchell.lang.e_quipmentshop.R;
-import mitchell.lang.e_quipmentshop.ui.Rule;
+import mitchell.lang.e_quipmentshop.ui.JavaBean.Rule;
 
 
 public class rulesFragment extends Fragment {
@@ -31,12 +31,33 @@ public class rulesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_rules, container, false);
+        ArrayList<Rule> rules = new ArrayList<>();
+        rules.add(new Rule("Offside", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Icing", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Boarding", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Charging", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Elbowing", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("High-Sticking", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Cross-Checking", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Holding", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Hooking", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Interference", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Roughing", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Slashing", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Misconduct", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Spearing", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Tripping", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+        rules.add(new Rule("Washout", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate purus est, vitae condimentum orci vestibulum eget. Vestibulum fringilla feugiat."));
+
         RecyclerView recyclerView = view.findViewById(R.id.ruleRecyclerView);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        CustomRecyclerViewAdapter adapter =
+                new CustomRecyclerViewAdapter(rules);
 
-        ArrayList<Rule> ruleArrayList = new ArrayList<>();
+        recyclerView.setAdapter(adapter);
 
-       return view;
+        return view;
     }
 
 }
