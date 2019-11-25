c46bfd36844e34b212c461da77b2827601f90419
Mitch <mitchlang444@gmail.com>
2019-11-24 19:43:08 -0500

    Seekbar added for age

--- app/src/main/java/mitchell/lang/e_quipmentshop/ui/stickpick/stickPickFragment.java
+++ app/src/main/java/mitchell/lang/e_quipmentshop/ui/stickpick/stickPickFragment.java
@@ -9,22 +9,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import mitchell.lang.e_quipmentshop.R;
 
 
 public class stickPickFragment extends Fragment {
-
+    SeekBar seekBar;
+    TextView textView;
 
     public stickPickFragment() {
         // Required empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stick_pick, container, false);
+        View view = inflater.inflate(R.layout.fragment_stick_pick, container, false);
+        SeekBar seekBar = view.findViewById(R.id.seekBar);
+
+        // Initialize the textview with '0'.
+        //textView.setText("Covered: " + seekBar.getProgress() + "/" + seekBar.getMax());
+        return view;
     }
 
 }
+
